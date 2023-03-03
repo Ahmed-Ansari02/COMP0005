275ba8f66af158baf8c3e16982d27bd7b3c5ef77
ninaadk07 <88612539+ninaadk07@users.noreply.github.com>
2023-03-03 16:47:43 +0000

    search still doesnt 100% work

--- Red Black Tree/balancedtreefirstdraft.py
+++ Red Black Tree/balancedtreefirstdraft.py
@@ -163,13 +163,15 @@ if __name__ == "__main__":
         tree = redblacktree()
 
 
-        tree.insert(10)
-        tree.insert(20)
-        tree.insert(30)
+        tree.insert(3)
+        tree.insert(1)
         tree.insert(5)
-        tree.insert(4)
-        tree.insert(2)
-
+        tree.insert(7)
+        tree.insert(6)
+        tree.insert(8)
+        tree.insert(9)
+        tree.insert(10)
+        
         tree.display()
 
 
